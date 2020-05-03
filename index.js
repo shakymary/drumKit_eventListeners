c43d3f5d768e3d3c29803ca2e462ef2d968bd453
Shakila Marando <marandoshakila1@gmail.com>
2020-05-02 19:57:15 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// document.querySelector("button").addEventListener("click", function () {
+
+
+//     alert("I got clicked");
+// });
+
+for (var i = 0; 1 < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        alert("I got clicked");
+
+
+
+    });
+
+
+    // for (var i=0; 1<document.querySelectorAll(".drum").length;i++
+
+    // var getAllBtn=Math.floor.length(i++)
+}
