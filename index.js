87c3a9800059727752388471968af72316224de3
Shakila Marando <marandoshakila1@gmail.com>
2020-05-03 15:03:07 -0700

    added some more features

--- index.js
+++ index.js
@@ -6,14 +6,20 @@
 
 for (var i = 0; 1 < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        alert("I got clicked");
 
+        this.style.color = "white";
+        var audio = new Audio('sounds/tom-3.mp3');
+        audio.play();
+
+        // document.querySelector(".first").addEventListener("click", function () {
+        //     var audio = new Audio('sounds/tom-3.mp3');
+        //     audio.play();
+
+        // document.querySelectorAll(".a drum")[i].addEventListener("click", function () {
+        //     var audio = new Audio('sounds/crash.mp3');
+        //     audio.play();
 
 
     });
 
-
-    // for (var i=0; 1<document.querySelectorAll(".drum").length;i++
-
-    // var getAllBtn=Math.floor.length(i++)
 }
